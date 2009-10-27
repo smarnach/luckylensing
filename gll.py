133b71fe6af65baa828ac7986823d314bb863179
Sven Marnach <sven@marnach.net>
2009-10-27 16:18:52 +0100

    Add functionality to convolve the pattern with a Gaussian kernel

--- gll.py
+++ gll.py
@@ -5,55 +5,54 @@ import luckylens as ll
 import pyconsole
 import threading
 import gobject
-from time import sleep
 
 xpixels = 1024
 ypixels = 512
 
 class RayshootThread(threading.Thread):
-    def __init__(self, magpat, app):
+    def __init__(self, magpat, callback):
         super(RayshootThread, self).__init__()
         self.magpat = magpat
-        self.app = app
+        self.progress1 = ll.Progress();
+        self.progress2 = ll.Progress();
+        self.callback = callback
 
-    def update_img(self, buf):
-        self.box.remove(self.progress_box)
-        self.box.add(self.img)
-        imgmap = gtk.gdk.Pixmap(self.box.window, xpixels, ypixels)
-        imgmap.draw_gray_image(self.box.get_style().fg_gc[gtk.STATE_NORMAL],
-                               0, 0, xpixels, ypixels, gtk.gdk.RGB_DITHER_NONE, buf)
-        self.app.imgbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, xpixels, ypixels)
-        self.app.imgbuf.get_from_drawable(imgmap, imgmap.get_colormap(), 0, 0, 0, 0, xpixels, ypixels)
-        self.box.queue_resize()
-        self.app.m = 0
-
-    def update_progressbar(self):
-        self.bar.set_fraction(min((self.progress1.value + self.progress1.value)*0.5, 1.0))
-
-    def init_progressbar(self):
-        self.bar = self.app.builder.get_object("progressbar")
-        self.box = self.app.builder.get_object("magpat_box")
-        self.img = self.app.builder.get_object("magpat")
-        self.progress_box = self.app.builder.get_object("progress_box")
-        self.box.remove(self.img)
-        self.box.add(self.progress_box)
+    def progress(self):
+        return (self.progress1.value + self.progress2.value)/2
 
     def run(self):
-        gobject.idle_add(self.init_progressbar)
         rect = ll.Rect(-1., -.25, 1.5, 0.)
-        self.progress1 = ll.Progress();
-        t1 = threading.Thread(target=ll.rayshoot, args=(self.magpat, rect, 100, 10, 3, self.progress1))
+        t1 = threading.Thread(target=ll.rayshoot,
+                              args=(self.magpat, rect, 50, 5, 3, self.progress1))
         t1.start()
         rect = ll.Rect(-1., 0., 1.5, .25)
-        self.progress2 = ll.Progress();
-        t2 = threading.Thread(target=ll.rayshoot, args=(self.magpat, rect, 100, 10, 3, self.progress2))
+        t2 = threading.Thread(target=ll.rayshoot,
+                              args=(self.magpat, rect, 50, 5, 3, self.progress2))
         t2.start()
-        while t1.isAlive() or t2.isAlive():
-            sleep(.05)
-            gobject.idle_add(self.update_progressbar)
-        buf = numpy.zeros((xpixels, ypixels), numpy.uint8)
+        t1.join()
+        t2.join()
+        buf = numpy.empty((ypixels, xpixels), numpy.uint8)
         ll.image_from_magpat(buf, self.magpat)
-        gobject.idle_add(self.update_img, buf)
+        gobject.idle_add(self.callback, buf)
+
+class ConvolveThread(threading.Thread):
+    def __init__(self, magpat, count, callback):
+        super(ConvolveThread, self).__init__()
+        self.magpat = magpat
+        self.count = count
+        self.callback = callback
+
+    def run(self):
+        kernel = numpy.indices((ypixels/2, xpixels/2))
+        kernel = numpy.exp(-sum(kernel*kernel)/8.)
+        kernel = numpy.concatenate((kernel, numpy.flipud(kernel)), 0)
+        kernel = numpy.concatenate((kernel, numpy.fliplr(kernel)), 1)
+        kernel /= numpy.sum(kernel)
+        self.count[:] = numpy.fft.irfft2(
+            numpy.fft.rfft2(self.count) *numpy.fft.rfft2(kernel))
+        buf = numpy.empty((ypixels, xpixels), numpy.uint8)
+        ll.image_from_magpat(buf, self.magpat)
+        gobject.idle_add(self.callback, buf)
 
 class GllApp(object):
     def __init__(self):
@@ -62,15 +61,26 @@ class GllApp(object):
         self.builder.connect_signals(self)
         self.imgbuf = None
         self.m = 0
+        self.progressbar_active = False
         self.lens_list = self.builder.get_object("lens_list")
+        self.magpat_box = self.builder.get_object("magpat_box")
+        self.progressbar = self.builder.get_object("progressbar")
+        self.magpat_img = self.builder.get_object("magpat")
+        self.progress_box = self.builder.get_object("progress_box")
 
     def generate_pattern(self, *args):
         lens = (ll.Lens*3)(*map(tuple, self.lens_list))
         lenses = ll.Lenses(3, lens)
         region = ll.Rect(.26, -.05, .46, .05)
-        self.count = numpy.zeros((xpixels, ypixels), numpy.int)
-        magpat = ll.new_MagPattern(lenses, region, xpixels, ypixels, 0, 0, self.count)
-        RayshootThread(magpat, self).start()
+        self.count = numpy.zeros((ypixels, xpixels), numpy.int)
+        self.magpat = ll.new_MagPattern(lenses, region, xpixels, ypixels, 0, 0, self.count)
+        t = RayshootThread(self.magpat, self.update_img)
+        self.init_progressbar()
+        gobject.timeout_add(100, self.update_progressbar, t.progress)
+        t.start()
+
+    def convolve_pattern(self, *args):
+        ConvolveThread(self.magpat, self.count, self.update_img).start()
 
     def show_pattern(self, widget, *args):
         if self.imgbuf is None:
@@ -85,6 +95,28 @@ class GllApp(object):
         self.m = m
         imgscaled = self.imgbuf.scale_simple(m//ypixels, m//xpixels, gtk.gdk.INTERP_BILINEAR)
         self.builder.get_object("magpat").set_from_pixbuf(imgscaled)
+
+    def update_img(self, buf):
+        if self.progressbar_active:
+            self.progressbar_active = False
+            self.magpat_box.remove(self.progress_box)
+            self.magpat_box.add(self.magpat_img)
+        imgmap = gtk.gdk.Pixmap(self.magpat_box.window, xpixels, ypixels)
+        imgmap.draw_gray_image(self.magpat_box.get_style().fg_gc[gtk.STATE_NORMAL],
+                               0, 0, xpixels, ypixels, gtk.gdk.RGB_DITHER_NONE, buf)
+        self.imgbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, xpixels, ypixels)
+        self.imgbuf.get_from_drawable(imgmap, imgmap.get_colormap(), 0, 0, 0, 0, xpixels, ypixels)
+        self.magpat_box.queue_resize()
+        self.m = 0
+
+    def update_progressbar(self, fraction):
+        self.progressbar.set_fraction(min(fraction(), 1.0))
+        return self.progressbar_active
+
+    def init_progressbar(self):
+        self.magpat_box.remove(self.magpat_img)
+        self.magpat_box.add(self.progress_box)
+        self.progressbar_active = True
 
     def show_console(self, *args):
         window = gtk.Window()
@@ -107,8 +139,7 @@ class GllApp(object):
         self.lens_list[path][2] = float(new_text)
         return
 
-    def quit_app(self, *args):
-        gtk.main_quit()
+    quit_app=gtk.main_quit
 
 gobject.threads_init()
 app = GllApp()
