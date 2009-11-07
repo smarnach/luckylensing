0e2b335893be3b8a8182a0d90f10289c617934f7
Sven Marnach <sven@marnach.net>
2009-11-07 22:11:37 +0100

    Bugfix and extensions for testll
     * Use ll_init_magpattern_params to correctly set up param struct
     * Allocate memory on heap rather than on the stack
     * Measure and print computation times
     * Transform magnification pattern to an image
     * Optionally write image to a file

--- testll.c
+++ testll.c
@@ -1,20 +1,40 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "ll.c"
 
 #define XPIXELS 1024
 #define YPIXELS 512
 
-struct ll_lens_type lens[3] = {{0.0, 0.0,  1.},
-                               {1.2, 0.0,  2e-2},
-                               {1.2, 0.025, 4e-3}};
-struct ll_lenses_type lenses = {3, lens};
-struct ll_rect_type region = {.26, -.05, .46, .05};
-int magpat[XPIXELS*YPIXELS];
-
-int main()
+int main(int argc, char *argv[])
 {
-    struct ll_magpattern_param_type params
-        = {lenses, region, XPIXELS, YPIXELS, 0.0, 0.0};
+    const unsigned N = XPIXELS*YPIXELS;
+    struct ll_lens_type lens[3] = {{0.0, 0.0,  1.},
+                                   {1.2, 0.0,  2e-2},
+                                   {1.2, 0.025, 4e-3}};
+    struct ll_lenses_type lenses = {3, lens};
+    struct ll_rect_type region = {.26, -.05, .46, .05};
+    struct ll_magpattern_param_type params;
+    ll_init_magpattern_params(&params, &lenses, &region, XPIXELS, YPIXELS);
     struct ll_rect_type rect = {-1., -.25, 1.5, .25};
     double progress;
-    ll_rayshoot(&params, magpat, &rect, 250, 50, 2, &progress);
+    int *magpat = calloc(N, sizeof(int));
+    char *buf = calloc(N, sizeof(char));
+
+    printf("Calculating magnification pattern...\n");
+    clock_t t = clock();
+    ll_rayshoot(&params, magpat, &rect, 300, 60, 3, &progress);
+    printf("finished in %g seconds.\n", (double)(clock()-t)/CLOCKS_PER_SEC);
+
+    printf("Converting to an image...\n");
+    t = clock();
+    ll_image_from_magpat(buf, magpat, N);
+    printf("finished in %g seconds.\n", (double)(clock()-t)/CLOCKS_PER_SEC);
+
+    if (argc > 1)
+    {
+        FILE *f = fopen(argv[1], "w");
+        fwrite(buf, sizeof(char), N, f);
+        fclose(f);
+    }
 }
