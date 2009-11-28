ef4f0b2e7817d572d0fa1d396659c2467b709e59
Sven Marnach <sven@marnach.net>
2009-11-28 10:59:05 +0100

    Some interface cosmetics

--- gll.py
+++ gll.py
@@ -20,44 +20,47 @@ class GllApp(object):
         self.lens_list = self.builder.get_object("lens_list")
         self.vbox = self.builder.get_object("vbox")
         self.statusbar = self.builder.get_object("statusbar")
+        self.progressbar = self.builder.get_object("progressbar")
 
-        self.progressbar = gtk.ProgressBar()
-        self.progressbar.set_property("show-text", True)
-        self.progressbar.show()
-
-        params = ll.MagPatternParams(self.lens_list, (.26, -.1, .46, .1),
-                                     xpixels, ypixels)
-        self.magpattern = GllMagPattern(params)
-        self.vbox.pack_start(self.magpattern.main_widget())
-        self.convolve = GllConvolve(self.magpattern)
-        self.mode = 1
+        self.mode = 0
 
     def generate_pattern(self, *args):
-        if self.mode != 1:
+        params = ll.MagPatternParams(self.lens_list, (-.005, -.01, .015, .01),
+                                     xpixels, ypixels)
+#        params = ll.MagPatternParams(self.lens_list, (.26, -.1, .46, .1),
+#                                     xpixels, ypixels)
+        if self.mode == 1:
+            self.vbox.remove(self.magpattern.main_widget())
+        elif self.mode == 2:
             self.vbox.remove(self.convolve.main_widget())
-            self.vbox.pack_start(self.magpattern.main_widget())
-            self.mode = 1
-        self.statusbar.add(self.progressbar)
-        gobject.timeout_add(100, self.update_progressbar, self.magpattern)
+        self.magpattern = GllMagPattern(params)
+        self.magpattern.density = 5
+        self.vbox.pack_start(self.magpattern.main_widget())
+        self.mode = 1
+        self.init_progressbar(self.magpattern)
         threading.Thread(target=self.magpattern.start).start()
 
     def convolve_pattern(self, *args):
+        self.convolve = GllConvolve(self.magpattern)
         if self.mode != 2:
             self.vbox.remove(self.magpattern.main_widget())
             self.vbox.pack_start(self.convolve.main_widget())
             self.mode = 2
-        self.statusbar.add(self.progressbar)
-        gobject.timeout_add(100, self.update_progressbar, self.convolve)
+        self.init_progressbar(self.convolve)
         threading.Thread(target=self.convolve.start).start()
 
     def extract_light_curve(self, *args):
         pass
 
+    def init_progressbar(self, comp):
+        self.progressbar.set_property("show-text", True)
+        gobject.timeout_add(100, self.update_progressbar, comp)
+
     def update_progressbar(self, comp):
         if comp.is_running():
             self.progressbar.set_fraction(min(comp.get_progress(), 1.0))
         else:
-            self.statusbar.remove(self.progressbar)
+            self.progressbar.set_property("show-text", False)
             self.progressbar.set_fraction(0.0)
             return False
         return True
