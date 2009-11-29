9907c56dca292e638d642dc7c00644c0e6106e82
Sven Marnach <sven@marnach.net>
2009-11-29 12:35:54 +0100

    Add GUI elements for setting parameters and interactive zooming

--- gll.py
+++ gll.py
@@ -3,13 +3,8 @@ import gobject
 import threading
 import gtk
 import pyconsole
-import numpy
-import luckylens as ll
 from magpattern import GllMagPattern
 from convolve import GllConvolve
-
-xpixels = 1024
-ypixels = 1024
 
 class GllApp(object):
     def __init__(self):
@@ -17,26 +12,15 @@ class GllApp(object):
         self.builder.add_from_file("gll.glade")
         self.builder.connect_signals(self)
 
-        self.lens_list = self.builder.get_object("lens_list")
-        self.vbox = self.builder.get_object("vbox")
+        self.hpaned = self.builder.get_object("hpaned")
         self.statusbar = self.builder.get_object("statusbar")
         self.progressbar = self.builder.get_object("progressbar")
 
-        self.mode = 0
+        self.magpattern = GllMagPattern()
+        self.hpaned.pack1(self.magpattern.main_widget(), resize=True)
+        self.hpaned.pack2(self.magpattern.config_widget(), resize=False)
 
     def generate_pattern(self, *args):
-        params = ll.MagPatternParams(self.lens_list, (-.005, -.01, .015, .01),
-                                     xpixels, ypixels)
-#        params = ll.MagPatternParams(self.lens_list, (.26, -.1, .46, .1),
-#                                     xpixels, ypixels)
-        if self.mode == 1:
-            self.vbox.remove(self.magpattern.main_widget())
-        elif self.mode == 2:
-            self.vbox.remove(self.convolve.main_widget())
-        self.magpattern = GllMagPattern(params)
-        self.magpattern.density = 5
-        self.vbox.pack_start(self.magpattern.main_widget())
-        self.mode = 1
         self.init_progressbar(self.magpattern)
         threading.Thread(target=self.magpattern.start).start()
 
@@ -75,16 +59,6 @@ class GllApp(object):
         swin.add(console)
         window.set_default_size(500, 400)
         window.show_all()
-
-    def edit_lens_cell0(self, cell, path, new_text):
-        self.lens_list[path][0] = float(new_text)
-        return
-    def edit_lens_cell1(self, cell, path, new_text):
-        self.lens_list[path][1] = float(new_text)
-        return
-    def edit_lens_cell2(self, cell, path, new_text):
-        self.lens_list[path][2] = float(new_text)
-        return
 
     def quit_app(self, *args):
         self.magpattern.cancel()
--- magpattern.py
+++ magpattern.py
@@ -61,12 +61,69 @@ class MagPattern(ll.Rayshooter):
         return None
 
 class GllMagPattern(MagPattern):
-    def __init__(self, params):
+    def __init__(self):
+        params = ll.MagPatternParams()
         super(GllMagPattern, self).__init__(params)
         self.imageview = gtkimageview.ImageView()
         self.scrollwin = gtkimageview.ImageScrollWin(self.imageview)
         self.scrollwin.show_all()
+        self.dragger = self.imageview.get_tool()
+        self.selector = gtkimageview.ImageToolSelector(self.imageview)
+        self.imageview.connect("button-press-event", self.imageview_clicked)
+        self.selector.connect("selection-changed", self.update_selection)
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("magpattern.glade")
+        self.builder.connect_signals(self)
+        self.lens_list = self.builder.get_object("lens_list")
+        self.fix_adjustments()
+        self.set_params_from_ui()
         self.running = False
+
+    def imageview_clicked(self, widget, event, data=None):
+        widget.grab_focus()
+        if event.button == 2:
+            widget.set_tool(self.dragger)
+        if event.button == 3:
+            widget.set_tool(self.selector)
+
+    def update_selection(self, selector):
+        rect = selector.get_selection()
+        params = self.params[0]
+        region = params.region
+        width = max(rect.width, rect.height*params.xpixels/params.ypixels)
+        x = rect.x + (rect.width - width)/2
+        xfactor = (region.x1-region.x0)/params.xpixels
+        x0 = region.x0 + x * xfactor
+        x1 = x0 + width * xfactor
+        height = max(rect.height, rect.width*params.ypixels/params.xpixels)
+        y = rect.y + (rect.height - height)/2
+        yfactor = (region.y1-region.y0)/params.ypixels
+        y0 = region.y0 + y * yfactor
+        y1 = y0 + width * yfactor
+        self.builder.get_object("region_x0").set_value(x0)
+        self.builder.get_object("region_y0").set_value(y0)
+        self.builder.get_object("region_x1").set_value(x1)
+        self.builder.get_object("region_y1").set_value(y1)
+
+    def fix_adjustments(self):
+        self.builder.get_object("xpixels").set_value(1024)
+        self.builder.get_object("ypixels").set_value(1024)
+        self.builder.get_object("region_x0").set_value(-.1)
+        self.builder.get_object("region_y0").set_value(-.3)
+        self.builder.get_object("region_x1").set_value( .5)
+        self.builder.get_object("region_y1").set_value( .3)
+        self.builder.get_object("density").set_value(100)
+
+    def set_params_from_ui(self):
+        params = self.params[0]
+        params.lenses = ll.Lenses(self.lens_list)
+        params.xpixels = int(self.builder.get_object("xpixels").get_value())
+        params.ypixels = int(self.builder.get_object("ypixels").get_value())
+        params.region.x0 = self.builder.get_object("region_x0").get_value()
+        params.region.y0 = self.builder.get_object("region_y0").get_value()
+        params.region.x1 = self.builder.get_object("region_x1").get_value()
+        params.region.y1 = self.builder.get_object("region_y1").get_value()
+        self.density = self.builder.get_object("density").get_value()
 
     def is_running(self):
         return self.running
@@ -74,8 +131,22 @@ class GllMagPattern(MagPattern):
     def main_widget(self):
         return self.scrollwin
 
+    def config_widget(self):
+        return self.builder.get_object("config")
+
+    def edit_lens_cell0(self, cell, path, new_text):
+        self.lens_list[path][0] = float(new_text)
+        return
+    def edit_lens_cell1(self, cell, path, new_text):
+        self.lens_list[path][1] = float(new_text)
+        return
+    def edit_lens_cell2(self, cell, path, new_text):
+        self.lens_list[path][2] = float(new_text)
+        return
+
     def start(self):
         self.running = True
+        self.set_params_from_ui()
         super(GllMagPattern, self).start()
         if self.cancel_flag:
             return
@@ -83,5 +154,6 @@ class GllMagPattern(MagPattern):
         ll.image_from_magpat(buf, self.count)
         self.pixbuf = gtk.gdk.pixbuf_new_from_array(buf.repeat(3, axis=2),
                                                     gtk.gdk.COLORSPACE_RGB, 8)
+        gobject.idle_add(self.imageview.set_tool, self.dragger)
         gobject.idle_add(self.imageview.set_pixbuf, self.pixbuf)
         self.running = False
