64d4d0c9bc3c2c1886f3af78f2ddb1a4076e88fd
Sven Marnach <sven@marnach.net>
2010-08-15 13:28:22 +0100

    Try to improve the color gradient for the animation
    It is hard to have the caustics clearly visible through the whole
    animation.  This is an attempt to improve the visibility for planet
    positions close to the Einstein radius.

--- examples/animation.py
+++ examples/animation.py
@@ -6,12 +6,12 @@ sys.path.append("../libll")
 import luckylensing as ll
 from rayshooter import Rayshooter
 
-colors = [(0, 0, 0), (0, 0, 255), (32, 0, 255),
+colors = [(0, 0, 0), (0, 0, 0), (0, 0, 255), (32, 0, 255),
           (255, 0, 0), (255, 255, 0), (255, 255, 255)]
-steps = [255, 32, 128, 255, 255]
+steps = [128, 512, 64, 255, 255, 512]
 
-for i in range(81):
-    x = 0.8 + i*0.005
+for i in range(101):
+    x = 0.8 + i*0.004
     p = ll.MagPatternParams([(0,0,1), (x, 0, .0004)], (-.5, -.25, 1, .5),
                             1024, 512)
     rs = Rayshooter(p)
