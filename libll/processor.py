540245021b6792b4197179efd8d28ec4c2eb9c02
Sven Marnach <sven@marnach.net>
2010-08-21 16:36:25 +0100

    Processor now fully supports history restore
    To this end, the methods restore() and restrict_history() have been
    added.

--- libll/processor.py
+++ libll/processor.py
@@ -1,30 +1,38 @@
 class Processor(object):
     def __init__(self):
         super(Processor, self).__init__()
-        self.history = []
+        self.history = {}
+        self.newest_serial = -1
 
     def get_input_keys(self, data):
         raise NotImplementedError
 
-    def needs_update(self, data, data_serials, cached_serial):
-        newest_input = -1
+    def needs_update(self, data, data_serials):
+        newest_input = 0
         for key in self.get_input_keys(data):
             if key in data and data_serials[key] > newest_input:
                 newest_input = data_serials[key]
-        return cached_serial < newest_input
+        return self.newest_serial < newest_input
 
     def update(self, data, data_serials=None, serial=None):
         if data_serials is None:
             data.update(self.run(data))
             return
-        if (not self.history or
-            self.needs_update(data, data_serials, self.history[-1][0])):
-            out_serial, output = serial, self.run(data)
-            self.history.append((out_serial, output))
+        if self.needs_update(data, data_serials):
+            output = self.run(data)
+            self.newest_serial = serial
         else:
-            out_serial, output = self.history[-1]
+            output = self.history[self.newest_serial]
+        self.history[serial] = output
         data.update(output)
-        data_serials.update(dict.fromkeys(output, out_serial))
+        data_serials.update(dict.fromkeys(output, self.newest_serial))
+
+    def restore(self, data, serial):
+        data.update(self.history[serial])
+
+    def restrict_history(self, serials):
+        self.history = dict((s, self.history[s])
+                            for s in serials if s in self.history)
 
     def run(self, data):
         return {}
