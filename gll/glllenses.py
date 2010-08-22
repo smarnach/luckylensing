f8ae213438fb3a93a04651a76cfb3a3c124936f1
Sven Marnach <sven@marnach.net>
2010-08-22 13:32:16 +0100

    Make lens list optionally export the shooting region

--- gll/glllenses.py
+++ gll/glllenses.py
@@ -15,16 +15,32 @@ class GllLenses(GllPlugin):
         self.lens_list = self.builder.get_object("lens_list")
         self.lens_selection = self.builder.get_object("treeview").get_selection()
         self.lens_selection.set_mode(gtk.SELECTION_MULTIPLE)
-        self.config_widget = self.builder.get_object("frame")
+        self.config_widget = self.builder.get_object("config")
+        self.set_config({"export_region": True,
+                         "region_x0": -.1,
+                         "region_y0": -.3,
+                         "region_x1":  .5,
+                         "region_y1":  .3})
 
     def get_config(self):
         d = {}
+        exp_reg = self.builder.get_object("export_region").get_active()
+        d["export_region"] = exp_reg
+        if exp_reg:
+            for key in ["region_x0", "region_x1", "region_y0", "region_y1"]:
+                d[key] = self.builder.get_object(key).get_value()
         all_lenses = map(tuple, self.lens_list)
         d["all_lenses"] = all_lenses
         d["lenses"] = [lens[1:] for lens in all_lenses if lens[0]]
         return d
 
     def set_config(self, config):
+        exp_reg = config["export_region"]
+        self.builder.get_object("export_region").set_active(exp_reg)
+        self.toggle_export_region()
+        if exp_reg:
+            for key in ["region_x0", "region_x1", "region_y0", "region_y1"]:
+                self.builder.get_object(key).set_value(config[key])
         all_lenses = config.get("all_lenses")
         if all_lenses is not None:
             self.lens_list.clear()
@@ -45,3 +61,9 @@ class GllLenses(GllPlugin):
         lens_list, selected = self.lens_selection.get_selected_rows()
         for row_path in reversed(selected):
             lens_list.remove(lens_list.get_iter(row_path))
+
+    def toggle_export_region(self, *args):
+        state = self.builder.get_object("export_region").get_active()
+        for name in ["x0", "x1", "y0", "y1"]:
+            self.builder.get_object("label_" + name).set_sensitive(state)
+            self.builder.get_object("spinbutton_" + name).set_sensitive(state)
--- gll/gllrayshooter.py
+++ gll/gllrayshooter.py
@@ -22,12 +22,14 @@ class GllRayshooter(GllPlugin):
         self.builder.connect_signals(self)
         self.set_config({"xpixels": 1024,
                          "ypixels": 1024,
-                         "region_x0": -.1,
-                         "region_y0": -.3,
-                         "region_x1":  .5,
-                         "region_y1":  .3,
                          "density": 100,
-                         "kernel": ll.KERNEL_TRIANGULATED})
+                         "num_threads": 2,
+                         "kernel": ll.KERNEL_TRIANGULATED,
+                         "export_region": False,
+                         "region_x0": -1.,
+                         "region_y0": -1.,
+                         "region_x1":  1.,
+                         "region_y1":  1.})
         self.region = None
         self.xpixels = None
         self.ypixels = None
@@ -35,10 +37,12 @@ class GllRayshooter(GllPlugin):
         self.config_widget = self.builder.get_object("config")
 
     def get_config(self):
-        d = {"num_threads": 2}
+        d = {}
         self.imageview.grab_focus()
         d["xpixels"] = int(self.builder.get_object("xpixels").get_value())
         d["ypixels"] = int(self.builder.get_object("ypixels").get_value())
+        exp_reg = self.builder.get_object("export_region").get_active()
+        d["export_region"] = exp_reg
         if self.region and self.imageview.get_tool() is self.selector:
             rect = self.selector.get_selection()
             width = max(rect.width, rect.height*self.xpixels/self.ypixels)
@@ -52,9 +56,13 @@ class GllRayshooter(GllPlugin):
             d["region_y1"] = self.region["y1"] - y * yfactor
             d["region_y0"] = d["region_y1"] - height * yfactor
         else:
-            for key in ["region_x0", "region_x1", "region_y0", "region_y1"]:
-                d[key] = self.builder.get_object(key).get_value()
+            if exp_reg:
+                for key in ["region_x0", "region_x1",
+                            "region_y0", "region_y1"]:
+                    d[key] = self.builder.get_object(key).get_value()
         d["density"] = self.builder.get_object("density").get_value()
+        d["num_threads"] = int(self.builder.get_object("num_threads"
+                                                       ).get_value())
         if self.builder.get_object("kernel_simple").get_active():
             d["kernel"] = ll.KERNEL_SIMPLE
         elif self.builder.get_object("kernel_bilinear").get_active():
@@ -66,9 +74,14 @@ class GllRayshooter(GllPlugin):
     def set_config(self, config):
         self.builder.get_object("xpixels").set_value(config["xpixels"])
         self.builder.get_object("ypixels").set_value(config["ypixels"])
+        exp_reg = config["export_region"]
+        self.builder.get_object("export_region").set_active(exp_reg)
+        self.toggle_export_region()
         for key in ["region_x0", "region_x1", "region_y0", "region_y1"]:
-            self.builder.get_object(key).set_value(config[key])
+            if key in config:
+                self.builder.get_object(key).set_value(config[key])
         self.builder.get_object("density").set_value(config["density"])
+        self.builder.get_object("num_threads").set_value(config["num_threads"])
         if config["kernel"] == ll.KERNEL_SIMPLE:
             self.builder.get_object("kernel_simple").set_active(True)
         elif config["kernel"] == ll.KERNEL_BILINEAR:
@@ -102,3 +115,9 @@ class GllRayshooter(GllPlugin):
                 widget.set_tool(self.selector)
             else:
                 widget.set_tool(self.dragger)
+
+    def toggle_export_region(self, *args):
+        state = self.builder.get_object("export_region").get_active()
+        for name in ["x0", "x1", "y0", "y1"]:
+            self.builder.get_object("label_" + name).set_sensitive(state)
+            self.builder.get_object("spinbutton_" + name).set_sensitive(state)
