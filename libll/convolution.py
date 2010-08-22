c7c739a23933a223d07271703a551f4e73b842fe
Sven Marnach <sven@marnach.net>
2010-08-22 23:23:24 +0100

    Simple Anti-aliasing for the edges of the flat source star profile

--- libll/convolution.py
+++ libll/convolution.py
@@ -6,8 +6,10 @@ class Convolution(Processor):
         return ["magpat", "source_fft"]
 
     def run(self, data):
+        kernel_fft = data.get("source_fft")
         magpat = data["magpat"]
-        kernel_fft = data["source_fft"]
+        if kernel_fft is None:
+            return {"convolved_magpat": magpat}
         convolved_pattern = numpy.empty_like(magpat)
         convolved_pattern[:] = numpy.fft.irfft2(
             numpy.fft.rfft2(magpat) * kernel_fft)
--- libll/sourcestar.py
+++ libll/sourcestar.py
@@ -1,5 +1,6 @@
 import numpy
 from processor import Processor
+from math import sqrt
 
 class SourceStar(Processor):
     def get_input_keys(self, data):
@@ -11,15 +12,18 @@ class SourceStar(Processor):
         ypixels = data["ypixels"]
         assert xpixels % 2 == 0
         assert ypixels % 2 == 0
-        source_radius = data.get("source_radius", 0.001)
+        source_radius = data.get("source_radius", 0.01)
         source_radius2 = source_radius*source_radius
         width = (data["region_x1"] - data["region_x0"]) / xpixels
         width2 = width*width
         height = (data["region_y1"] - data["region_y0"]) / ypixels
         height2 = height*height
-        kernel = numpy.indices((ypixels/2, xpixels/2))
+        kernel = numpy.indices((ypixels/2, xpixels/2)) + 0.5
         kernel = kernel*kernel
-        kernel = self.f(height2*kernel[0]+width2*kernel[1], source_radius2)
+        kernel = self.f(height2*kernel[0]+width2*kernel[1], source_radius2,
+                        width, height)
+        if kernel is None:
+            return {"source_fft": None}
         kernel = numpy.concatenate((kernel, numpy.flipud(kernel)), 0)
         kernel = numpy.concatenate((kernel, numpy.fliplr(kernel)), 1)
         kernel = kernel / float(numpy.sum(kernel))
@@ -27,9 +31,20 @@ class SourceStar(Processor):
         return {"source_fft": kernel_fft}
 
 class FlatSource(SourceStar):
-    def f(self, r2, source_radius2):
-        return r2 < source_radius2
+    def f(self, r2, source_radius2, width, height):
+        width2 = width*width
+        height2 = height*height
+        if source_radius2 <= 0.25*width2 + 0.25*height2:
+            return None
+        if source_radius2 <= 0.5*width2 + 0.5*height2:
+            return r2 < source_radius2
+        source_radius = sqrt(source_radius2)
+        m = 1.0/((width+height)*source_radius)
+        b = (source_radius2 +
+             0.5*(width+height)*source_radius +
+             0.25*width*height)
+        return numpy.clip(-m*r2 + m*b, 0.0, 1.0)
 
 class GaussianSource(SourceStar):
-    def f(self, r2, source_radius2):
+    def f(self, r2, source_radius2, width, height):
         return numpy.exp(-r2 / (2.0*source_radius2))
