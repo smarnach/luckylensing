3de8bfd5111ef8771e57ea284cdcc740e3d2a440
Sven Marnach <sven@marnach.net>
2010-08-24 15:59:25 +0100

    Some parameter tweaks in the cluster movie example

--- examples/cluster-movie.sh
+++ examples/cluster-movie.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-for i in `seq 0 3000`; do
+for i in `seq 0 3141`; do
     ./cluster.py imgfile="magpats/cluster-$(printf %04i $i).png" \
-        kernel=2 density=625 xpixels=1024 ypixels=768 \
+        kernel=bilinear density=500 xpixels=1024 ypixels=768 \
         region_x0=-0.3 region_y0=-0.225 region_x1=0.3 region_y1=0.225 \
-        random_seed=47 angle="eval(0.0005*$i)" min_mag=1.1 max_mag=60.0
+        random_seed=47 angle="eval(0.0005*$i)" min_mag=1.25 max_mag=60.0
 done
