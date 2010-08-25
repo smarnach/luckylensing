e5a258532cb2c65db519fe211f2c918407ba9aff
Sven Marnach <sven@marnach.net>
2010-08-25 02:33:00 +0100

    GUI: Set more convenient step sizes in globular cluster config

--- gll/gllglobularcluster.py
+++ gll/gllglobularcluster.py
@@ -9,10 +9,10 @@ class GllGlobularCluster(GllPlugin):
         self.config_widget = GllConfigBox(
             [("num_stars", "Number of stars", (1000, 0, 100000, 1), 0),
              ("random_seed", "Random seed", (42, -1000000, 1000000, 1), 0),
-             ("log_mass", "Log of mean mass", (-7.0, -50.0, 50.0, 1.0), 2),
+             ("log_mass", "Log of mean mass", (-7.0, -50.0, 50.0, 0.1), 2),
              ("log_mass_stddev", "Log(mass) std dev", (0.1, 0.0, 50.0, 0.1), 2),
              ("angle", "Rotation angle", (0.0, -100.0, 100.0, 0.01), 4),
-             ("region_radius", "Region radius", (1.0, 0.0, 1000.0, 1.0), 2)])
+             ("region_radius", "Region radius", (1.0, 0.0, 1000.0, 0.1), 2)])
 
     def get_config(self):
         config = self.config_widget.get_config()
