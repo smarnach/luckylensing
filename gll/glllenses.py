fecf70b7720fe6e125d41f2c866310f9ca428d9a
Sven Marnach <sven@marnach.net>
2010-08-25 14:49:47 +0100

    GUI: GllLenses uses GllConfigBox now

--- gll/glllenses.py
+++ gll/glllenses.py
@@ -1,5 +1,6 @@
 import gtk
 from gllplugin import GllPlugin
+from gllconfigbox import GllConfigBox
 
 class GllLenses(GllPlugin):
     def __init__(self):
@@ -11,32 +12,24 @@ class GllLenses(GllPlugin):
         self.lens_list = self.builder.get_object("lens_list")
         self.lens_selection = self.builder.get_object("treeview").get_selection()
         self.lens_selection.set_mode(gtk.SELECTION_MULTIPLE)
-        self.config_widget = self.builder.get_object("config")
-        self.set_config({"export_region": True,
-                         "region_x0": -1.,
-                         "region_y0": -1.,
-                         "region_x1":  1.,
-                         "region_y1":  1.})
+        self.config_widget = GllConfigBox()
+        self.config_widget.add_toggle_block(
+            "export_region", "Ray shooting region", False,
+            [("region_x0", "Left coordinate",  (-1.0, -1e10, 1e10, 0.01), 4),
+             ("region_y0", "Lower coordinate", (-1.0, -1e10, 1e10, 0.01), 4),
+             ("region_x1", "Right coordinate", ( 1.0, -1e10, 1e10, 0.01), 4),
+             ("region_y1", "Upper coordinate", ( 1.0, -1e10, 1e10, 0.01), 4)])
+        self.config_widget.add(self.builder.get_object("lens_box"))
 
     def get_config(self):
-        d = {}
-        exp_reg = self.builder.get_object("export_region").get_active()
-        d["export_region"] = exp_reg
-        if exp_reg:
-            for key in ["region_x0", "region_x1", "region_y0", "region_y1"]:
-                d[key] = self.builder.get_object(key).get_value()
+        config = self.config_widget.get_config()
         all_lenses = map(tuple, self.lens_list)
-        d["all_lenses"] = all_lenses
-        d["lenses"] = [lens[1:] for lens in all_lenses if lens[0]]
-        return d
+        config["all_lenses"] = all_lenses
+        config["lenses"] = [lens[1:] for lens in all_lenses if lens[0]]
+        return config
 
     def set_config(self, config):
-        exp_reg = config["export_region"]
-        self.builder.get_object("export_region").set_active(exp_reg)
-        self.toggle_export_region()
-        if exp_reg:
-            for key in ["region_x0", "region_x1", "region_y0", "region_y1"]:
-                self.builder.get_object(key).set_value(config[key])
+        self.config_widget.set_config(config)
         all_lenses = config.get("all_lenses")
         if all_lenses is not None:
             self.lens_list.clear()
@@ -57,9 +50,3 @@ class GllLenses(GllPlugin):
         lens_list, selected = self.lens_selection.get_selected_rows()
         for row_path in reversed(selected):
             lens_list.remove(lens_list.get_iter(row_path))
-
-    def toggle_export_region(self, *args):
-        state = self.builder.get_object("export_region").get_active()
-        for name in ["x0", "x1", "y0", "y1"]:
-            self.builder.get_object("label_" + name).set_sensitive(state)
-            self.builder.get_object("spinbutton_" + name).set_sensitive(state)
