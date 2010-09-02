b5ea11c7f848ce818d76cb5bd0fc09259dfa4882
Sven Marnach <sven@marnach.net>
2010-09-02 18:06:21 +0100

    GUI: Call the expose handler of GllImageView only if necessary

--- gll/gllimageview.py
+++ gll/gllimageview.py
@@ -8,23 +8,23 @@ class GllImageView(gtkimageview.ImageScrollWin):
         self.imageview = gtkimageview.ImageView()
         self.imageview.set_interpolation(gtk.gdk.INTERP_TILES)
         self.imageview.connect("button-press-event", self.imageview_clicked)
-        self.imageview.connect("expose-event", self.expose)
         super(GllImageView, self).__init__(self.imageview)
         self.show_all()
-        self.dirty = False
+        self.handler = None
 
     def mark_dirty(self):
-        self.dirty = True
         self.imageview.queue_draw()
+        if self.handler is None:
+            self.handler = self.imageview.connect("expose-event", self.expose)
 
     def imageview_clicked(self, widget, event, data=None):
         widget.grab_focus()
 
     def expose(self, *args):
-        if self.dirty:
-            self.dirty = False
-            pixbuf = self.get_pixbuf()
-            if isinstance(pixbuf, numpy.ndarray):
-                pixbuf = gtk.gdk.pixbuf_new_from_array(
-                    pixbuf, gtk.gdk.COLORSPACE_RGB, 8)
-            self.imageview.set_pixbuf(pixbuf)
+        pixbuf = self.get_pixbuf()
+        if isinstance(pixbuf, numpy.ndarray):
+            pixbuf = gtk.gdk.pixbuf_new_from_array(
+                pixbuf, gtk.gdk.COLORSPACE_RGB, 8)
+        self.imageview.set_pixbuf(pixbuf, False)
+        self.imageview.disconnect(self.handler)
+        self.handler = None
