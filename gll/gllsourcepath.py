2681814a44de87e7e1bce2366ab9759fb31c6004
Sven Marnach <sven@marnach.net>
2010-09-02 17:31:04 +0100

    GUI: Add ImageToolSourcePath to draw the source path

--- gll/gllsourcepath.py
+++ gll/gllsourcepath.py
@@ -3,6 +3,47 @@ import numpy
 from gllplugin import GllPlugin
 from gllconfigbox import GllConfigBox
 from gllimageview import GllImageView
+import gtk
+import gtkimageview
+import gobject
+
+class ImageToolSourcePath(gobject.GObject, gtkimageview.IImageTool):
+    def __init__(self):
+        gobject.GObject.__init__(self)
+        self.crosshair = gtk.gdk.Cursor(gtk.gdk.CROSSHAIR)
+        self.cache = gtkimageview.PixbufDrawCache()
+
+    def do_button_press(self, event):
+        pass
+
+    def do_button_release(self, event):
+        pass
+
+    def do_motion_notify(self, event):
+        pass
+
+    def do_pixbuf_changed(self, reset_fit, rect):
+        pass
+
+    def do_paint_image(self, opts, drawable):
+        self.cache.draw(opts, drawable)
+        x0 = self.coords[0] * opts.zoom - opts.zoom_rect.x + opts.widget_x
+        y0 = self.coords[1] * opts.zoom - opts.zoom_rect.y + opts.widget_y
+        x1 = self.coords[2] * opts.zoom - opts.zoom_rect.x + opts.widget_x
+        y1 = self.coords[3] * opts.zoom - opts.zoom_rect.y + opts.widget_y
+        ctx = drawable.cairo_create()
+        ctx.rectangle(opts.widget_x, opts.widget_y,
+                      opts.zoom_rect.width, opts.zoom_rect.height)
+        ctx.clip()
+        ctx.set_source_rgb(1.0, 1.0, 1.0)
+        ctx.move_to(x0, y0)
+        ctx.line_to(x1, y1)
+        ctx.stroke()
+
+    def do_cursor_at_point(self, x, y):
+        return self.crosshair
+
+gobject.type_register(ImageToolSourcePath)
 
 class GllSourcePath(GllPlugin):
     name = "Source path"
@@ -10,6 +51,8 @@ class GllSourcePath(GllPlugin):
     def __init__(self):
         super(GllSourcePath, self).__init__()
         self.main_widget = GllImageView(self.get_pixbuf)
+        self.tool = ImageToolSourcePath()
+        self.main_widget.imageview.set_tool(self.tool)
         self.config_widget = GllConfigBox(
             [("curve_x0", "Start x coordinate", (-1.0, -1e10, 1e10, 0.01), 4),
              ("curve_y0", "Start y coordinate", (0.0, -1e10, 1e10, 0.01), 4),
@@ -39,23 +82,15 @@ class GllSourcePath(GllPlugin):
     def update_coords(self, data):
         pixels_per_width = self.xpixels / (self.region_x1 - self.region_x0)
         pixels_per_height = self.ypixels / (self.region_y1 - self.region_y0)
-        self.coords = ((data["curve_x0"] - self.region_x0) * pixels_per_width,
-                       (self.region_y1 - data["curve_y0"]) * pixels_per_height,
-                       (data["curve_x1"] - self.region_x0) * pixels_per_width,
-                       (self.region_y1 - data["curve_y1"]) * pixels_per_height)
+        self.tool.coords = (
+            (data["curve_x0"] - self.region_x0) * pixels_per_width,
+            (self.region_y1 - data["curve_y0"]) * pixels_per_height,
+            (data["curve_x1"] - self.region_x0) * pixels_per_width,
+            (self.region_y1 - data["curve_y1"]) * pixels_per_height)
         self.main_widget.mark_dirty()
 
     def get_pixbuf(self):
-        buf = numpy.ascontiguousarray(numpy.dstack(
-            (self.magpat_buf, 255*numpy.ones_like(self.magpat_buf[...,:1]))))
-        surface = cairo.ImageSurface.create_for_data(
-            buf.data, cairo.FORMAT_ARGB32, self.xpixels, self.ypixels)
-        ctx = cairo.Context(surface)
-        ctx.set_source_rgb(1.0, 1.0, 1.0)
-        ctx.move_to(self.coords[0], self.coords[1])
-        ctx.line_to(self.coords[2], self.coords[3])
-        ctx.stroke()
-        return buf
+        return self.magpat_buf
 
     def coords_changed(self, *args):
         if hasattr(self, "magpat_buf"):
