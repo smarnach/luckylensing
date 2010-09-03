e68eba5722e6eb283f15fac05098605d360e48e1
Sven Marnach <sven@marnach.net>
2010-09-03 13:32:17 +0100

    GUI: User cairo also in light curve tool

--- gll/glllightcurve.py
+++ gll/glllightcurve.py
@@ -2,14 +2,16 @@ import gtk
 from lightcurve import LightCurve
 from gllplugin import GllPlugin
 from gllconfigbox import GllConfigBox
-from gllimageview import GllImageView
 
 class GllLightCurve(GllPlugin):
     name = "Light curve"
 
     def __init__(self):
         super(GllLightCurve, self).__init__(LightCurve())
-        self.main_widget = GllImageView(self.get_pixbuf)
+        self.main_widget = gtk.DrawingArea()
+        self.main_widget.set_size_request(400, 300)
+        self.main_widget.show()
+        self.main_widget.connect("expose-event", self.draw)
         self.config_widget = GllConfigBox()
         self.config_widget.add_toggle_block(
             "export_max", "Set upper magnification", False,
@@ -22,24 +24,26 @@ class GllLightCurve(GllPlugin):
         self.config_widget.set_config(config)
 
     def update(self, data):
-        self.xpixels = data["xpixels"]
-        self.ypixels = data["ypixels"]
         curve = data["light_curve"]
-        samples = len(curve)
-        max_mag = data.get("curve_max_mag", curve.max()*1.2)
-        self.points = [(int(i*(self.xpixels-1)/(samples-1)),
-                        int((self.ypixels-1)*(1.0-mag/max_mag)))
-                       for i, mag in enumerate(curve) if mag]
-        self.main_widget.mark_dirty()
+        self.max_mag = data.get("curve_max_mag", curve.max()*1.1)
+        self.num_samples = len(curve)
+        self.points = [(i, mag) for i, mag in enumerate(curve) if mag]
 
-    def get_pixbuf(self):
-        pixmap = gtk.gdk.Pixmap(None, self.xpixels, self.ypixels, 24)
-        gc = self.main_widget.style.bg_gc[gtk.STATE_NORMAL]
-        pixmap.draw_rectangle(gc, True, 0, 0, self.xpixels, self.ypixels)
-        gc = self.main_widget.style.fg_gc[gtk.STATE_NORMAL]
-        pixmap.draw_lines(gc, self.points)
-        pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False,
-                                8, self.xpixels, self.ypixels)
-        pixbuf.get_from_drawable(pixmap, pixmap.get_colormap(),
-                                 0, 0, 0, 0, self.xpixels, self.ypixels)
-        return pixbuf
+    def draw(self, area, event):
+        if not len(self.points):
+            return
+        cr = area.window.cairo_create()
+        cr.rectangle(event.area.x, event.area.y,
+                      event.area.width, event.area.height)
+        cr.clip()
+        matrix = cr.get_matrix()
+        width, height = area.window.get_size()
+        cr.translate(0.1 * width, 0.9 * height)
+        cr.scale(0.8*width / (self.num_samples-1), -0.8*height / self.max_mag)
+        cr.rectangle(0.0, 0.0, self.num_samples-1, self.max_mag)
+        cr.clip_preserve()
+        cr.move_to(*self.points[0])
+        for p in self.points[1:]:
+            cr.line_to(*p)
+        cr.set_matrix(matrix)
+        cr.stroke()
