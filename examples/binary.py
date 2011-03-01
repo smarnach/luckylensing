7cbb19dbbc844c79a21e1d9a270ed5584fae67ca
Sven Marnach <sven@marnach.net>
2011-03-01 12:10:11 +0000

    Refactor binary.py to use procedural interface and PIL

--- examples/binary.py
+++ examples/binary.py
@@ -5,32 +5,34 @@
 import sys
 sys.path.append("..")
 
-from luckylensing import Rayshooter, pipeline
-from imagewriter import save_img
-from subprocess import call
+from luckylensing import rayshoot
+from PIL import Image
 import numpy
 
+# Parameters to control the output ####
 x_values = 0.6 + 0.1*numpy.arange(12)
 mass_values = 1. / 10.**(0.5*numpy.arange(7))
+region = (-0.5, -1.0, 1.5, 1.0)
+xpixels = ypixels = 256
+margin = 8
+imgfile = "magpats/binary.png"
+#######################################
 
-pipe = pipeline.Pipeline(Rayshooter, save_img)
+shape = (len(mass_values) * (ypixels + 2 * margin),
+         len(x_values) * (ypixels + 2 * margin), 3)
+buf = numpy.zeros(shape, numpy.uint8)
 for j, mass in enumerate(mass_values):
     for i, x in enumerate(x_values):
-        pipe.run(
-            # Lens configuration
-            lenses = [(0., 0., 1.), (x, 0., mass)],
-            # Parameters for the ray shooter:
-            region = (-0.5, -1., 1.5, 1.),
-            xpixels = 256,
-            ypixels = 256,
-            density = 1000,
-            num_threads = 2,
-            # Parameters for the image file writer:
-            min_mag = 1.0,
-            max_mag = 120.0,
-            imgfile = "binary-%02i-%02i.png" % (j, i))
-
-s = call("montage binary-??-??.png -geometry +8+8 -tile %ix " % len(x_values) +
-         "-background black magpats/binary.png", shell=True)
-if not s:
-    call("rm binary-??-??.png", shell=True)
+        print "Ray shooting for mass ratio %.2e and distance %.2f" % (mass, x)
+        sys.stdout.write("\033[F") # Cursor up one line
+        lenses = [(0.0, 0.0, 1.0), (x, 0.0, mass)]
+        magpat = rayshoot(lenses, region, xpixels, ypixels,
+                          density=1000, num_threads=2)
+        j0 = j * (ypixels + 2 * margin)
+        i0 = i * (xpixels + 2 * margin)
+        buf[j0:j0+ypixels, i0:i0+xpixels] = magpat.render_gradient(
+            min_mag=1.0, max_mag=120.0)
+sys.stdout.write("\033[K") # Clear to the end of line
+print "Writing magnification pattern to", imgfile
+Image.fromarray(buf).save(imgfile)
+print "Done."
