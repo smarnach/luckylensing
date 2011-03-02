1304ca2ac738f6bb363be3f8738a115362d4c6ca
Sven Marnach <sven@marnach.net>
2011-03-02 13:31:59 +0000

    Simplify parse_args() function and move it to cluster.py

--- examples/cluster.py
+++ examples/cluster.py
@@ -5,9 +5,24 @@
 import sys
 sys.path.append("..")
 
-from argsparser import parse_args
 from luckylensing import pipeline, globular_cluster, Rayshooter
 from imagewriter import save_img
+
+def parse_args(args):
+    """Parse command line arguments in the format "key=value".
+
+    The function returns a dictionary of the key/value pairs.  Values
+    are evaluated as Python expressions using eval() if possible.
+    """
+    output = {}
+    for arg in args:
+        key, value = arg.split("=", 1)
+        try:
+            value = eval(value)
+        except Exception:
+            pass
+        output[key] = value
+    return output
 
 pipe = pipeline.Pipeline(parse_args, globular_cluster, Rayshooter, save_img)
 pipe.run(args=sys.argv[1:], num_threads=2)
